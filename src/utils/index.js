2628825db3e41d0dca58a412b6aff75a42116d54
22jair <jair_55_88@hotmail.com>
2020-11-14 19:15:50 -0500

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 //Local
 // const URL_API = "http://localhost:5000/api/";
 // AWS
-const URL_API = "https://flickrr-env.eba-c3mb3ca2.us-east-1.elasticbeanstalk.com/api/";
+const URL_API = "http://flickrr-env.eba-c3mb3ca2.us-east-1.elasticbeanstalk.com/api/";
 
 export const URL_API_USER = URL_API+'users/';
 export const URL_API_IMG = URL_API+'imgs/';
